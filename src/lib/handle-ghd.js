ee2b102d333e34d3ee729c2d9a30f4914f1e8642
oger000 <gerhard.oettl@ogersoft.at>
2021-06-28 20:07:49 +0200

    import 1997

--- src/lib/handle-ghd.js
+++ src/lib/handle-ghd.js
@@ -236,49 +236,49 @@ async function importTxt1997(config, data) {
 
     case '03':   // schulden
       tableName = 'vermoegenshaushalt'
-      rec.ansatz_uab= line.substr()
-      rec.ansatz_ugl= line.substr()
-      rec.konto_grp= line.substr()
-      rec.konto_ugl= line.substr()
-      rec.sonst_ugl= line.substr()
-      rec.vorhabencode= line.substr()
-      rec.id_vhh= line.substr()
-      rec.sektor= line.substr()
+      rec.ansatz_uab= line.substr(43, 3)
+      rec.ansatz_ugl= line.substr(46, 3)
+      // rec.konto_grp= line.substr()
+      // rec.konto_ugl= line.substr()
+      // rec.sonst_ugl= line.substr()
+      // rec.vorhabencode= line.substr()
+      rec.id_vhh= line.substr(23, 15).trim()
+      // rec.sektor= line.substr()
       rec.land= line.substr(47, 2)
 
-      rec.mvag_vhh= line.substr()
-      rec.ansatz_text= line.substr()
-      rec.konto_text= line.substr()
+      // rec.mvag_vhh= line.substr()
+      // rec.ansatz_text= line.substr()
+      rec.konto_text= line.substr(216, 80)
       rec.endstand_vj= parseInt(line.substr(66, 14)) / 100
       rec.zugang= parseInt(line.substr(80, 14)) / 100
       rec.abgang= parseInt(line.substr(94, 14)) / 100
       rec.aenderung= parseInt(line.substr(108, 14)) / 100
-
       rec.endstand_rj= parseInt(line.substr(122, 14)) / 100
-      rec.abschreibung= line.substr()
-      rec.umbuchung= line.substr()
-      rec.hoehe= line.substr()
+
+      // rec.abschreibung= line.substr()
+      // rec.umbuchung= line.substr()
+      rec.hoehe= parseInt(line.substr(164, 14)) / 100
       rec.ersaetze= parseInt(line.substr(150, 14)) / 100
       rec.zinsen= parseInt(line.substr(136, 14)) / 100
-      rec.verzinsungsart= line.substr()
+      rec.verzinsungsart= line.substr(215, 1)
       rec.waehrung= line.substr(51, 3)
       rec.laufzeit_von= line.substr(178, 6)
       rec.laufzeit_bis= line.substr(184, 6)
-      rec.fbn= line.substr()
-      rec.isin= line.substr()
-      rec.notleidend= line.substr()
-      rec.minleasing= line.substr()
-      rec.nachweis= line.substr()
-      rec.wechselkurs_zug= line.substr()
-      rec.wechselkurs_vj= line.substr()
-      rec.wechselkurs_rj= line.substr()
-      rec.zinsanpassungstermin= line.substr()
+      // rec.fbn= line.substr()
+      rec.isin= line.substr(54, 12)
+      // rec.notleidend= line.substr()
+      // rec.minleasing= line.substr()
+      // rec.nachweis= line.substr()
+      // rec.wechselkurs_zug= line.substr()
+      // rec.wechselkurs_vj= line.substr()
+      // rec.wechselkurs_rj= line.substr()
+      // rec.zinsanpassungstermin= line.substr()
 
-      rec.bonitaet= line.substr()
-      rec.ausfallrisiko= line.substr()
+      // rec.bonitaet= line.substr()
+      // rec.ausfallrisiko= line.substr()
       rec.zinssatz= parseInt(line.substr(190, 5)) / 1000
       rec.refzinssatz= line.substr(195, 20)
-      rec.minzinssatz= line.substr()
+      // rec.minzinssatz= line.substr()
       rec.endstand_fj0= parseInt(line.substr(296, 14)) / 100
       rec.endstand_fj1= parseInt(line.substr(310, 14)) / 100
       rec.endstand_fj2= parseInt(line.substr(324, 14)) / 100
@@ -287,15 +287,206 @@ async function importTxt1997(config, data) {
       // rec.endstand_fj5= parseInt(line.substr(, 14)) / 100
       break
 
-    case '04':
-    case '05':
-    case '06':
-    case '07':
-    case '08':
-    case '09':
-    case '91':
-      tableName = 'dummy'
+    case '04':   // vermögen und forderungen
+      tableName = 'vermoegenshaushalt'
+      rec.ansatz_uab= line.substr(38, 3)
+      rec.ansatz_ugl= line.substr(41, 3)
+      // rec.konto_grp= line.substr()
+      // rec.konto_ugl= line.substr()
+      // rec.sonst_ugl= line.substr()
+      // rec.vorhabencode= line.substr()
+      rec.id_vhh= line.substr(23, 15).trim()
+      rec.sektor= line.substr(50, 2)
+      rec.land= line.substr(52, 2)
+
+      // rec.mvag_vhh= line.substr()
+      // rec.ansatz_text= line.substr()
+      rec.konto_text= line.substr(198, 80).trim()
+      rec.endstand_vj= parseInt(line.substr(70, 14)) / 100
+      rec.zugang= parseInt(line.substr(84, 14)) / 100
+      rec.abgang= parseInt(line.substr(98, 14)) / 100
+      rec.aenderung= parseInt(line.substr(112, 14)) / 100
+      rec.endstand_rj= parseInt(line.substr(126, 14)) / 100
+
+      // rec.abschreibung= line.substr()
+      // rec.umbuchung= line.substr()
+      // rec.hoehe= line.substr()
+      rec.ersaetze= parseInt(line.substr(140, 14)) / 100  // einnahmen aus guthaben
+      // rec.zinsen= line.substr()
+      // rec.verzinsungsart= line.substr()
+      rec.waehrung= line.substr(54, 3)
+      rec.laufzeit_von= line.substr(154, 6)
+      rec.laufzeit_bis= line.substr(160, 6)
+      rec.fbn= line.substr(191, 7)
+      rec.isin= line.substr(58, 12)
+      rec.notleidend= line.substr(57, 1)
+      // rec.minleasing= line.substr()
+      // rec.nachweis= line.substr()
+      // rec.wechselkurs_zug= line.substr()
+      // rec.wechselkurs_vj= line.substr()
+      // rec.wechselkurs_rj= line.substr()
+      // rec.zinsanpassungstermin= line.substr()
+
+      // rec.bonitaet= line.substr()
+      // rec.ausfallrisiko= line.substr()
+      rec.zinssatz= parseInt(line.substr(166, 5)) / 1000
+      rec.refzinssatz= line.substr(171, 20).trim()
+      // rec.minzinssatz= line.substr()
+      // rec.endstand_fj0= line.substr()
+      // rec.endstand_fj1= line.substr()
+      // rec.endstand_fj2= line.substr()
+      // rec.endstand_fj3= line.substr()
+      // rec.endstand_fj4= line.substr()
+      // rec.endstand_fj5= line.substr()
       break
+
+    case '05':   // übrige vermögenskonten
+      tableName = 'vermoegenshaushalt'
+      rec.ansatz_uab= line.substr(38, 3)
+      rec.ansatz_ugl= line.substr(41, 3)
+      // rec.konto_grp= line.substr()
+      // rec.konto_ugl= line.substr()
+      // rec.sonst_ugl= line.substr()
+      // rec.vorhabencode= line.substr()
+      rec.id_vhh= line.substr(23, 15).trim()
+      // rec.sektor= line.substr(50, 2)
+      // rec.land= line.substr(52, 2)
+
+      // rec.mvag_vhh= line.substr()
+      // rec.ansatz_text= line.substr()
+      rec.konto_text= line.substr(120, 80).trim()
+      rec.endstand_vj= parseInt(line.substr(50, 14)) / 100
+      rec.zugang= parseInt(line.substr(64, 14)) / 100
+      rec.abgang= parseInt(line.substr(78, 14)) / 100
+      rec.aenderung= parseInt(line.substr(92, 14)) / 100
+      rec.endstand_rj= parseInt(line.substr(106, 14)) / 100
+
+      // rec.abschreibung= line.substr()
+      // rec.umbuchung= line.substr()
+      // rec.hoehe= line.substr()
+      // rec.ersaetze= parseInt(line.substr(140, 14)) / 100  // einnahmen aus guthaben
+      // rec.zinsen= line.substr()
+      // rec.verzinsungsart= line.substr()
+      // rec.waehrung= line.substr(54, 3)
+      // rec.laufzeit_von= line.substr(154, 6)
+      // rec.laufzeit_bis= line.substr(160, 6)
+      // rec.fbn= line.substr(191, 7)
+      // rec.isin= line.substr(58, 12)
+      // rec.notleidend= line.substr(57, 1)
+      // rec.minleasing= line.substr()
+      // rec.nachweis= line.substr()
+      // rec.wechselkurs_zug= line.substr()
+      // rec.wechselkurs_vj= line.substr()
+      // rec.wechselkurs_rj= line.substr()
+      // rec.zinsanpassungstermin= line.substr()
+
+      // rec.bonitaet= line.substr()
+      // rec.ausfallrisiko= line.substr()
+      // rec.zinssatz= parseInt(line.substr(166, 5)) / 1000
+      // rec.refzinssatz= line.substr(171, 20).trim()
+      // rec.minzinssatz= line.substr()
+      // rec.endstand_fj0= line.substr()
+      // rec.endstand_fj1= line.substr()
+      // rec.endstand_fj2= line.substr()
+      // rec.endstand_fj3= line.substr()
+      // rec.endstand_fj4= line.substr()
+      // rec.endstand_fj5= line.substr()
+      break
+
+    case '06':
+      tableName = 'personal'
+      rec.ansatz_uab= line.substr(24, 3)
+      rec.ansatz_ugl= line.substr(26, 3)
+      rec.meldegruppe= line.substr(23, 1)
+      rec.personenkreis= line.substr(30, 1)
+      rec.vzae= parseInt(line.substr(31, 14)) / 100
+      rec.koepfe= parseInt(line.substr(45, 14)) / 100
+      // rec.vzae_fj0= line.substr()
+      // rec.koepfe_fj0= line.substr()
+      // rec.vzae_fj1= line.substr()
+      // rec.koepfe_fj1= line.substr()
+      break
+
+    case '07':
+      tableName = 'sonstige_daten'
+      let positionsnummer = line.substr(23, 3)
+      let wert = parseInt(line.substr(26, 14)) / 100
+      if (positionsnummer === '001') {
+        rec.hebesatz1 = wert
+      }
+      else if (positionsnummer === '002') {
+        rec.hebesatz2 = wert
+      }
+      else {
+        console.log(`Zeile ${lineNum} Satzart 07 - unbekannte positionsnummer ${positionsnummer}.`)
+      }
+      break
+
+    case '08':
+      tableName = 'beteiligungen'
+      rec.id= line.substr(23, 14)
+      // rec.id_vhh= line.substr()
+      rec.name_einheit= line.substr(175, 80).trim()
+      rec.adresse= line.substr(255, 40).trim()
+      rec.postleitzahl= line.substr(295, 4)
+      rec.ort= line.substr(299, 40)
+      rec.sektor= line.substr(57, 2)
+      rec.betverhaeltnis= line.substr(37, 1)
+      rec.id_mutter= line.substr(38, 14)
+      rec.betausmass= parseInt(line.substr(52, 5)) / 100
+      // rec.gjahr_von= line.substr()
+      rec.gjahr_bis= line.substr(59, 4) + '00-00'
+      rec.stammkapital= parseInt(line.substr(63, 14)) / 100
+      // rec.ekap_vj= line.substr()
+      rec.ekap_gj= parseInt(line.substr(63, 14)) / 100
+      // rec.bilanzsumme= line.substr()
+      rec.verbindl_gesamt= parseInt(line.substr(77, 14)) / 100
+      rec.verbindl_finanz= parseInt(line.substr(91, 14)) / 100
+      rec.verbindl_gk= parseInt(line.substr(105, 14)) / 100
+      rec.ueber_fehl= parseInt(line.substr(119, 14)) / 100
+      // rec.gewinnaus_gk= line.substr()
+      rec.vzae= parseInt(line.substr(147, 14)) / 100
+      rec.koepfe= parseInt(line.substr(161, 14)) / 100
+      // rec.guthaben= line.substr()
+      // rec.forderungen= line.substr()
+      // rec.gem_beitrag= line.substr()
+      break
+
+    case '09':
+      tableName = 'haftungen'
+      rec.id = line.substr()
+      rec.id_haftung = line.substr(23, 15).trim()
+      // rec.teil = line.substr()
+      // rec.gruppe = line.substr()
+      rec.haftungsnehmer = line.substr(156, 80).trim()
+      rec.sektor = line.substr(38, 2)
+      rec.laufzeit_von = line.substr(144, 6)
+      rec.laufzeit_bis = line.substr(150, 6)
+      // rec.solidar = line.substr()
+      rec.anteil = parseInt(line.substr(41, 5)) / 188
+      rec.haftungsrahmen = parseInt(line.substr(102, 14))  / 100
+      rec.endstand_vj = parseInt(line.substr(46, 14)) / 100
+      rec.zugang = parseInt(line.substr(60, 14))  / 100
+      rec.abgang = parseInt(line.substr(74, 14))  / 100
+      rec.endstand_rj = parseInt(line.substr(88, 14))  / 100
+      rec.endstand_fj1 = parseInt(line.substr(330, 14))  / 100
+      rec.endstand_fj2 = parseInt(line.substr(344, 14))  / 100
+      rec.endstand_fj3 = parseInt(line.substr(358, 14))  / 100
+      rec.endstand_fj4 = parseInt(line.substr(372, 14))  / 100
+      // rec.endstand_fj5 = line.substr()
+      break
+
+    case '91':
+      tableName = 'pension'
+      let positionsnummer =  = line.substr(23, 3)
+      // rec.fj_nr = line.substr()
+      // rec.aufwendungen = line.substr()
+      // rec.koepfe_r = line.substr()
+      // rec.koepfe_rl = line.substr()
+      // rec.koepfe_v = line.substr()
+      // rec.koepfe_vl = line.substr()
+      break
+
     default:
       console.log(`Unbekannte Satzart ${satzart}.`)
       continue
@@ -308,6 +499,12 @@ async function importTxt1997(config, data) {
     records[tableName].push(rec)
   }  // eo text line loop
 
+  // prep sonstige_daten
+  records.sonstige_daten = [ Object.assign(records.sonstige_daten[0], records.sonstige_daten[1]) ]
+
+  // TODO prep pension
+
+
   // write to db
   for (tableName of Object.keys(records)) {
     if (tableName === 'dummy') {
@@ -315,7 +512,7 @@ async function importTxt1997(config, data) {
     }
     await db(tableName).where(ghdWhereKeys).del()
     await db(tableName).insert(records[tableName])
-    console.log(`${tableName}: ${records[tableName].length} Datensätze geschrieben.`)
+    console.log(`${records[tableName].length} Datensätze in Tabelle ${tableName} geschrieben.`)
   }  // eo write part loop
 
 }  // eo import txt 1997
