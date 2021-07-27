c5392f498f2d7a5e199f02f8e0c6eaab415b1b40
oger000 <gerhard.oettl@ogersoft.at>
2021-07-27 23:03:18 +0200

    build

--- ogerghh/client/quasar.conf.js
+++ ogerghh/client/quasar.conf.js
@@ -47,7 +47,7 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
-      // publicPath: '/gemeindehaushalt', // see also index.template.html
+      publicPath: '/gemeindehaushalt', // see also index.template.html (maybe webpack5 has public path auto detection)
       // publicPath: 'auto',
 
       // transpile: false,
